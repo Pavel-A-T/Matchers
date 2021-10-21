a7e5afbac829013f3ae2ed2ddc0f6d65e017270e
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-10-22 00:09:20 +0300

    matchers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Matchers
-Matchers
+Matchers [![Build status](https://ci.appveyor.com/api/projects/status/plgs1tmfmtxx4hcd/branch/master?svg=true)](https://ci.appveyor.com/project/Pavel-A-T/matchers/branch/master)
+
---
+++ src/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import sortArray from '../index';
+
+const arr = [
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+];
+const sortedArr = [
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
+];
+
+test(('Сортировка по здоровью'), () => {
+  expect(sortArray(arr)).toEqual(sortedArr);
+});
